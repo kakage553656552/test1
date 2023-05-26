a52d6cf0a3bb3da6187c0b4421411f76fc376247
Kaka.zhang <Kaka.zhang@mspbots.ai>
2023-05-26 18:27:16 +0800

    111

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <el-button type="primary">home</el-button>
+    <div>111</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      msg: 'home',
+      aa: {
+        b: {
+          c: 999
+        }
+      }
+    }
+  },
+  created() {
+    // eslint-disable-next-line no-debugger
+    debugger
+    let _this = this
+    console.log(_this.aa.b.c)
+    console.log('home')
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  div {
+    color: red;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      // sass: {
+      //   data: `@import "@/styles/global.scss";` // 加载全局变量
+      // }
+    }
+  }
 })
