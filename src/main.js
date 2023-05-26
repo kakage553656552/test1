d61e3000bc3d57b174a6d1fd7270ac3999b63e53
kakage553656552 <40747362+kakage553656552@users.noreply.github.com>
2023-05-26 18:29:51 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/styles/global.scss';
 Vue.use(ElementUI);
-
+// test hooks
 Vue.config.productionTip = false
 
 new Vue({
